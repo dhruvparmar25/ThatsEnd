99b3aa7e124e66f51ad2582d0862adf9617e4df6
dhruvparmar25 <parmardhruv034@gmail.com>
2025-08-18 16:52:55 +0530

    ochi

--- ReactJS/learning/src/pages/Hooks/useCallBack/CallBack.jsx
+++ ReactJS/learning/src/pages/Hooks/useCallBack/CallBack.jsx
@@ -28,17 +28,21 @@ function CallBack() {
     const handelfillter = useCallback((item)=>
         item.toLowerCase().includes(query.toLowerCase()),[query])
 
+    // const handelfillter = (item)=>
+    //     item.toLowerCase().includes(query.toLowerCase())
     
 
 
     return (
-        <div style={{ display: 'flex', flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "1rem", margin: "0px", padding: "0px", width: "100%", height: "100vh", backgroundColor: theme === "light" ? "white" : "black", color: theme === "light" ? "black" : "white" }}>
-            <h1 style={{ margin: "0px", padding: "0px" }}>CallBack</h1>
-            <input value={query} onChange={(e) => setQuery(e.target.value)} />
-            <button onClick={() => {
+        <div style={{ display: 'flex', position:"relative", flexDirection: "column", alignItems: "center", justifyContent: "center", gap: "1rem", margin: "0px", padding: "0px", width: "100%", height: "100vh", backgroundColor: theme === "light" ? "white" : "black", color: theme === "light" ? "black" : "white" }}>
+           <div>
+                 <button style={{position:"absolute",right:"0",top:"0"}} onClick={() => {
                 setTheme(theme === "dark" ? "light" : "dark")
             }}>{theme}</button>
+            <h1 style={{ margin: "0px", padding: "0px" }}>CallBack</h1>
+            <input value={query} onChange={(e) => setQuery(e.target.value)} />
             <ChildCallBack item={arrItems} fillterData={handelfillter}/>
+            </div>
         </div>
     )
 }
